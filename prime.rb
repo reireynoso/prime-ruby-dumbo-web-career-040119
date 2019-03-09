0d240472deb41dd30c251d92b917b6bcba406671
Reinald Reynoso <reireynoso@gmail.com>
2019-03-09 05:17:32 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(integer)
+  if integer < 0 || integer == 0 || integer == 1
+    return false 
+  end 
+  range = integer
+  for d in (1..range)
+    if integer % d == 0
+      return true
+    else return false 
+    end
+  end
+end
