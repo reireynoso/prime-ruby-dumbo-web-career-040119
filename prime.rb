472df6a8c350105f0282a3a5e08c4e73597a705f
Reinald Reynoso <reireynoso@gmail.com>
2019-03-09 05:41:04 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(integer)
+  if integer <= 1 
+    return false 
+  elsif integer > 1 && integer <= 3
+    return true
+  end 
+  
+  i = 2
+  while i < integer do
+    if integer % i == 0 
+      return false
+    end
+    i += 1
+  end 
+  return true
+end
